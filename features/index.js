d081671478a9556ba8d4f9487fc8e81d85c8830a
celestialblood <mairapinetta95@gmail.com>
2021-05-24 21:44:57 -0300

    cambios2

--- features/index.js
+++ features/index.js
@@ -59,3 +59,41 @@ $(function() {
         });
     }
 });
+
+
+/*PAGINA 2 */
+$(function() {
+    $('#gif-click4').hide();
+
+    $('#click4').click(function() {
+        $('#click4').hide();
+        $('#gif-click4').show();
+
+    });
+
+    if ($('#click4').data("clicked", true)) {
+        $('#gif-click4').click(function() {
+            $('#click4').show();
+            $('#gif-click4').hide();
+        
+        });
+    }
+});
+
+$(function() {
+    $('#gif-click5').hide();
+
+    $('#click5').click(function() {
+        $('#click5').hide();
+        $('#gif-click5').show();
+
+    });
+
+    if ($('#click5').data("clicked", true)) {
+        $('#gif-click5').click(function() {
+            $('#click5').show();
+            $('#gif-click5').hide();
+        
+        });
+    }
+});
